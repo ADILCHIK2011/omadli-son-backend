f97d6b287dcbfa73286c4f62534e3b4ca3eac8aa
ADILCHIK2011 <kadirovadilchik@gmail.com>
2026-04-14 21:32:28 +0500

    efve

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const connectDB = require("./config/database.js")
 const userRouter = require("./routers/user.router.js")
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: '*'
-  }));
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 connectDB()
 app.use("/api/v1", userRouter)

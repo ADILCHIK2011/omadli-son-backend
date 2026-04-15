4a08f4bfcecf72e8e74bbbe3246eff8b3e27bc50
ADILCHIK2011 <kadirovadilchik@gmail.com>
2026-04-15 17:39:55 +0500

    asdaa

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const connectDB = require("./config/database.js")
-const userRouter = require("./routers/user.router.js")
+const connectDB = require("./config/database.js");
+const userRouter = require("./routers/user.router.js");
 
-const app = express()
+const app = express();
 
-const CORS_LINK = process.env.CORS || "https://omadli-son-frontend.vercel.app/"
+const CORS_LINK = process.env.CORS || "https://omadli-son-frontend.vercel.app";
 app.use(cors({
-    origin: CORS_LINK
-}
-));
+    origin: CORS_LINK.replace(/\/$/, ""),
+    credentials: true
+}));
 
+app.use(express.json());
 
-app.use(express.json())
+connectDB();
 
-connectDB()
-app.use("/api/v1", userRouter)
+app.use("/api/v1", userRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Nimadir xato ketdi!');
+});
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server ${PORT}-portda ishga tushdi`);
 });
